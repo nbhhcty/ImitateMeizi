ca4986290fcf67759e96fcb3e85b8ceb289cce13
吴树凯 <shukaiwu@pptv.com>
2018-07-27 19:08:32 +0800

    [Feature] 不知道什么文件

--- ImitateMeizi/Model/MeiziItem.swift
+++ ImitateMeizi/Model/MeiziItem.swift
@@ -15,13 +15,6 @@ struct MeiziIterm: Codable {
     var objectId:String = ""
     var thumb_url:String = ""
     var title:String = ""
-
-//    category = ZaHui;
-//    "group_url" = "https://www.dbmeinv.com:443/dbgroup/1736776";
-//    "image_url" = "https://wx3.sinaimg.cn/large/0060lm7Tgy1ftbzuqwn06j30dw0dv0u4.jpg";
-//    objectId = 5b4cc6d29f54540031fc0465;
-//    "thumb_url" = "https://wx3.sinaimg.cn/small/0060lm7Tgy1ftbzuqwn06j30dw0dv0u4.jpg";
-//    title = "\U9760\U8c31\U662f\U4ec0\U4e48\U5b9a\U4e49\U5462";
 }
 
 struct MeiziIterms: Codable {
